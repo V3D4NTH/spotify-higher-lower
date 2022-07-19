ec0938517dce5ceeb2f5c62712a55632a7cd83d1
Edward <edwardxiao29@gmail.com>
2022-07-19 14:19:40 -0400

    game over logic

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -6,70 +6,81 @@ import db2020 from '../data/2020data.json'
 
 
 export default function Game() {
-    const getRandomSong = (_not:number):number => {
+
+    const getRandomSong = (_not:number, __not:number):number => {
         let randomNumber = Math.floor(Math.random() * db2020.length)
-        if (randomNumber === _not)
-            return getRandomSong(_not)
+        if (randomNumber === _not || randomNumber === __not)
+            return getRandomSong(_not, __not)
         else
             return randomNumber
     }
 
     const checkWin = (High:boolean, valOne:number, valTwo:number):boolean => {
-
         if (High){
             return (valOne <= valTwo)
         } 
-
         else if (!High) {
             return (valOne >= valTwo)
         }
-
         return false
+    }
+    const UpdateGameOrGameOver = (High:boolean, valOne:number, valTwo:number):void => {
 
+        if (checkWin(High, valOne, valTwo) === true){
+            setCurrentScore(currentScore + 1)
+
+            let firstIndex = firstArrIndex
+            let secondIndex = secondArrIndex
+
+            setFirstArrIndex(secondIndex)
+            setSongOne(songTwo)
+            
+            const randomNum = getRandomSong(firstIndex, secondIndex)
+
+            setSecondArrIndex(randomNum)
+            setSongTwo(db2020[randomNum])
+            
+        }
+        else {
+            setGameOver(true)
+            if (currentScore > parseInt(window.localStorage.getItem("High Score")))
+                window.localStorage.setItem("High Score", currentScore.toString())
+        }
     }
 
     const [gameOver, setGameOver] = useState(false)
 
-    const [firstArrIndex, setFirstArrIndex] = useState(getRandomSong(-1))
+    const [firstArrIndex, setFirstArrIndex] = useState(getRandomSong(-1, -1))
     const [songOne, setSongOne] = useState(db2020[firstArrIndex])
 
-    const [secondArrIndex, setSecondArrIndex] = useState(getRandomSong(firstArrIndex))
+    const [secondArrIndex, setSecondArrIndex] = useState(getRandomSong(firstArrIndex, -1))
     const [songTwo, setSongTwo] = useState(db2020[secondArrIndex])
 
     const [highScore , setHighScore] = useState(window.localStorage.getItem("High Score") || 0)
     const [currentScore, setCurrentScore] = useState(0)
 
-    const UpdateGameOrGameOver = (High:boolean, valOne:number, valTwo:number):void => {
-
-        if (checkWin(High, valOne, valTwo) === true){
-            setCurrentScore(currentScore + 1)
-        }
-        else {
-            setGameOver(true)
-            if (currentScore > window.localStorage.getItem("High Score"))
-                window.localStorage.setItem("High Score", currentScore)
-        }
-    }
 
     return (
         <>
-            {"game over" ? gameOver===true : "game on"}
+            <div>
+                {gameOver && "gameOver"}
+            </div>
             <div>
                 Song 1: {songOne.SONGNAME} {' '}
-                Loudness: {songOne.loudness}
+                Loudness: {songOne.danceability}
             </div>
             <div>
                 Song 2: {songTwo.SONGNAME} {' '}
-                Loudness: {songTwo.loudness}
+                Loudness: {songTwo.danceability}
             </div>
             <div>
                 <button
-                    onClick={() => UpdateGameOrGameOver(true, songOne.loudness, songTwo.loudness)}
+                    onClick={() => UpdateGameOrGameOver(true, songOne.danceability, songTwo.danceability)}
                 >
                     Higher
                 </button>
                 <button
-                    onClick={() => UpdateGameOrGameOver(false, songOne.loudness, songTwo.loudness)}
+                    onClick={() => UpdateGameOrGameOver(false, songOne.danceability, songTwo.danceability)}
                 >
                     Lower
                 </button>
